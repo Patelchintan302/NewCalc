8ef22ed1c149c1cd70489cfb862b58af65ef2a4f
Patelchintan302 <patelcg302@gmail.com>
2025-07-28 19:35:11 +0530

    ongoing project

---
+++ app/src/main/java/com/example/newcalc/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newcalc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+}
